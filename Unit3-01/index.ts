69ecc5880d5ad7c4528e9804ddd8dfe7b36afecc
Kaitlyn Ip <kaitlyn.ip@mths.ca>
2024-06-03 18:32:33 +0000

    init 3-01

---
+++ Unit3-01/index.ts
@@ -0,0 +1,35 @@
+/*
+ * This is a program that shows recursion.
+ *
+ * @author  Kaitlyn Ip
+ * @version 1.0
+ * @since   2024-01-01
+ */
+
+// reverse inputted string with recursion
+function reverse_string(input_string: string): string {
+  if (input_string) {
+    const myChar = input_string.charAt(0)
+    input_string = input_string.slice(1, input_string.length)
+    input_string = reverse_string(input_string)
+    input_string += myChar
+  }
+  return input_string
+}
+
+let original_string = "recursion"
+console.log("original string: " + original_string)
+console.log("reversed string: " + reverse_string(original_string))
+console.log()
+
+original_string = ""
+console.log("original string: " + original_string)
+console.log("reversed string: " + reverse_string(original_string))
+console.log()
+
+original_string = "Room212"
+console.log("original string: " + original_string)
+console.log("reversed string: " + reverse_string(original_string))
+console.log()
+
+console.log("\nDone.")
---
+++ Unit3-01/java/ReverseString.java
@@ -0,0 +1,76 @@
+/*
+* This program shows recursion.
+*
+* @author  Kaitlyn Ip
+* @version 1.0
+* @since   2024-03-25
+*/
+
+/**
+* This runs testcases.
+*/
+final class ReverseString {
+
+    /**
+    * Prevent instantiation.
+    * Throw an exception IllegalStateException.
+    * if this is ever called
+    *
+    * @throws IllegalStateException if this is ever called
+    *
+    */
+    private ReverseString() {
+        throw new IllegalStateException("Cannot be instantiated");
+    }
+
+    /**
+    * The starting main() function.
+    *
+    * @param args No args will be used
+    */
+    public static void main(final String[] args) {
+        final String og = "original string: ";
+        final String reversed = "reversed string: ";
+        // input
+        String original_string = "recursion";
+        System.out.println(og + original_string);
+        System.out.println(
+            reversed + reverseString(original_string)
+        );
+        System.out.println();
+
+        original_string = "";
+        System.out.println(og + original_string);
+        System.out.println(
+            reversed + reverseString(original_string)
+        );
+        System.out.println();
+
+        original_string = "Room212";
+        System.out.println(og + original_string);
+        System.out.println(
+            reversed + reverseString(original_string)
+        );
+        System.out.println();
+
+        System.out.println("\nDone.");
+    }
+
+    /**
+    * The reverse string() function.
+    *
+    * @param input string that is inputted
+    *
+    * @return reversed string
+    */
+    public static String reverseString(String input) {
+        String input_string = input;
+        if (input_string.length() > 0) {
+            final char myChar = input_string.charAt(0);
+            input_string = input_string.substring(1, input_string.length());
+            input_string = reverse_string(input_string);
+            input_string += myChar;
+        }
+        return input_string;
+    }
+}
